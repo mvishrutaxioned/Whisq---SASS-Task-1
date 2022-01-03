9e8dcb4265abbdf18a33365a9e2e910f4751157a
VishrutMargam <vishrutm@axioned.com>
2022-01-03 16:51:33 +0530

    added toggleUp functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ $(document).ready(() => {
     // responsive navbar
     $('.menu, .close').on('click', e => {
         e.preventDefault();
-
         $('nav').toggleClass('showNav');
     })
 
@@ -18,4 +17,18 @@ $(document).ready(() => {
     $('.menu').on('click', () => $('html, body').css('overflow', 'hidden'));
     $('.close').on('click', () => $('html, body').css('overflow', 'visible'));
 
+    // hide and show toggle btn functioanality
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > topHeight) {
+            $('.toggleUp').addClass('flex');
+        } else {
+            $('.toggleUp').removeClass('flex');
+        }
+    });
+
+    // on toggleUp click functionality
+    $('.toggleUp').click(e => {
+        e.preventDefault();
+        window.scroll({top: 0, behavior: "smooth"});
+    })
 })
